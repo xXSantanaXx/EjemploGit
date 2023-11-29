82905718e33b192b1a35204cd5ada8aab39079e2
ale-l <ale-l@192.168.1.107>
2023-11-29 10:18:49 +0100

    Primera semana de trabajo

--- PruebaGIT/src/main/java/Principal/Principal.java
+++ PruebaGIT/src/main/java/Principal/Principal.java
@@ -3,6 +3,12 @@ package Principal;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("pene");
+		System.out.println("pene");
+		System.out.println("pene");
+		System.out.println("pene");
+		System.out.println("pene");
+		System.out.println("pene");
 		
 	}
 
