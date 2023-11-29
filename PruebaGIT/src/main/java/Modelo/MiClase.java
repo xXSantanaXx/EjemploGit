67ad86bfce3a418400963c7fd4df7dc12150e4b5
ale-l <ale-l@192.168.1.107>
2023-11-29 10:14:04 +0100

    Interfaces y modelo

---
+++ PruebaGIT/src/main/java/Modelo/MiClase.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+import Interfaces.MiInterface;
+
+public class MiClase implements MiInterface{
+
+	@Override
+	public int metodoDevuelveEntero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
